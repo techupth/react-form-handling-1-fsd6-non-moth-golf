5d12b5847d5ed033085e26ca8f1a891d01b6d46f
Moth <kampanee.moth@gmail.com>
2024-06-05 13:19:28 +0700

    feat: create function onChange and onClick with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [greetingInput, setGreetingInput] = useState("Greeting Message");
+  const [greetingMessage, setGreetingMessage] = useState("");
+
+  function onChange(event) {
+    setGreetingInput(event.target.value);
+  }
+
+  function onClick() {
+    setGreetingMessage(greetingInput);
+  }
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingMessage}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input id="greeting-message" type="text" onChange={onChange} />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={onClick}>Update text</button>
       </div>
     </div>
   );
